44dc72ca289ef9cfe8fba5c6870f64b7cb54ab55
MrJ-403 <135211406+MrJ-403@users.noreply.github.com>
2025-10-19 23:36:36 +0300

    Question 1. Interface with two methods (Borrow, Return). Abstraction, abcstact class (AbstractBook) implementing the Interface and providing one abstract method (GetGenre) *NOTE: FIX AFTER QUESTION 5 IS SOLVED AND PUSHED

---
+++ IPG203HW/IBookActions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPG203HW
+{
+    internal interface IBookActions
+    {
+        void Borrow();
+
+        void Return();
+    }
+}

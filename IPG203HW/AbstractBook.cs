1d9032e588ab7ff2861d345f9f2b90d01fed3f80
MrJ-403 <135211406+MrJ-403@users.noreply.github.com>
2025-10-19 23:36:36 +0300

    Question 1. Interface with two methods (Borrow, Return). Abstraction, abstract class (AbstractBook) implementing the Interface and providing one abstract method (GetGenre) *NOTE: FIX AFTER QUESTION 5 IS SOLVED AND PUSHED

---
+++ IPG203HW/AbstractBook.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPG203HW
+{
+    internal abstract class AbstractBook : IBookActions
+    {
+        private string isbn {get;}
+        private string title { get; set; }
+        private string author { get; set; }
+        private bool isBorrowed;
+
+        public AbstractBook(string ISBN, string Title, string Author)
+        {
+            //if (!BookValidator.IsValidISBN(ISBN)) use BookValidator in Question 5
+            //    throw new ArgumentException("Invalid ISBN for Book");
+            isbn = ISBN;
+            title = Title;
+            author = Author;
+            //LibraryStats.TotalBooksCount++; use LibraryStats in Question 5
+        }
+
+        public virtual void Borrow()
+        {
+            isBorrowed = true;
+            //LibraryStats.TotalBooksBorrowed++; use LibraryStats in Question 5
+            OnBookBorrowed?.Invoke($"{title} has been borrowed");
+        }
+
+        public virtual void Return()
+        {
+            isBorrowed = false;
+            //LibraryStats.TotalBooksBorrowed--; use LibraryStats in Question 5
+            OnBookReturned?.Invoke($"{title} has been returned");
+        }
+
+        public abstract string GetGenre();
+
+        public delegate void BookEventHandler(string message);
+        public event BookEventHandler OnBookBorrowed;
+        public event BookEventHandler OnBookReturned;
+    }
+}

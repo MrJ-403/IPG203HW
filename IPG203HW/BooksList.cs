6640a4a261748df0260c5249f6a50886eca5d171
mohammadyamen99 <yamenyamen0936440780@gmail.com>
2025-10-20 17:55:47 +0300

    Add files via upload

---
+++ IPG203HW/BooksList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPG203HW
+{
+    internal class BooksList
+    {
+        private List<AbstractBook> books;
+        
+        public BooksList()
+        {
+            books = new List<AbstractBook>();
+        }
+
+        public void AddBook(AbstractBook book) 
+        { 
+            books.Add(book);
+        }
+
+        public void RemoveBook(AbstractBook book)
+        {
+            books.Remove(book);
+        }
+
+        public void Clear()
+        {
+            books.Clear();
+        }
+
+        public void GetGenres()
+        {
+            foreach (AbstractBook book in books)
+            {
+                Console.WriteLine($"Title: {book.Title}, Genre: {book.GetGenre()}");
+            }
+        }
+    }
+}

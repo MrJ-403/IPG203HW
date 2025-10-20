89da33556af807ab9345b5ebac11438624d49425
zeinasharaf48-source <zeinasharaf48@gmail.com>
2025-10-20 21:21:02 +0300

    Add files via upload

---
+++ BookValidator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace IPG203HW
+{
+    internal static class BookValidator
+    {
+        public static bool IsValidISBN(string isbn)
+        {
+            return isbn.Length == 13 && isbn.All(char.IsDigit);
+        }
+
+        public static bool IsValidTitle(string title)
+        {
+            return !string.IsNullOrWhiteSpace(title) && title.Length <= 100;
+        }
+
+        public static bool IsValidAuthor(string author)
+        {
+            return !string.IsNullOrWhiteSpace(author) && author.All(char.IsLetterOrDigit);
+        }
+
+        public static bool IsValidPublicationYear(int year)
+        {
+            int currentYear = DateTime.Now.Year;
+            return year <= currentYear;
+        }
+
+    }
+}

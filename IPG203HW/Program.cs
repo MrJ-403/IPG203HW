2d19ee0a3912036b21d6bd9ebc2a156b6cbaa9bc
MrJ-403 <135211406+MrJ-403@users.noreply.github.com>
2025-10-21 11:40:19 +0300

    Fixed wrong spacename. Added a Usage example of the Classes

--- IPG203HW/Program.cs
+++ IPG203HW/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
-namespace HW
+namespace IPG203HW
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BooksList books = new BooksList();
+            FictionBook fictionBook = new FictionBook("1548745623145", "Space & Terraforming", "Steve Johnson");
+            ScienceBook scienceBook = new ScienceBook("1658736942516", "The Human Body", "Ahmad Radwan");
+            HistoryBook historyBook = new HistoryBook("2657913679823", "World War II", "Alexander H");
+
+            scienceBook.Borrow();
+            fictionBook.Borrow();
+            scienceBook.Return();
+
+            books.AddBook(fictionBook);
+            books.AddBook(scienceBook);
+            books.AddBook(historyBook);
+
+            books.GetGenres();
+            Console.WriteLine($"Total Books: {LibraryStats.TotalBooksCount} books\nTotal Borrowed Books: {LibraryStats.TotalBooksBorrowed} books");
+            
         }
     }
 }

774502725cbc168afce167a7f84c8ddd5267144f
Hanaa219 <badrhana236@gmail.com>
2025-10-21 11:09:21 +0300

    Add files via upload

---
+++ IPG203HW/ScienceBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IPG203HW
+{
+    internal class ScienceBook : AbstractBook
+    {
+        public ScienceBook(string ISBN, string Title, string Author)
+            : base(ISBN, Title, Author)
+        {
+        }
+
+        public override string GetGenre()
+        {
+            return "Science";
+        }
+    }
+}
